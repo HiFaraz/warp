4e8d23290f25ad1beb684373f0227a994abf5203
Faraz Syed <hello@farazsyed.com>
2018-02-24 16:15:02 -0500

    refactor: clean up event-loop.h

--- include/event-loop.h
+++ include/event-loop.h
@@ -1,29 +1,21 @@
 #ifndef EVENT_LOOP_H
 #define EVENT_LOOP_H
 
-#include <algorithm> // import std::foreach
 #include <functional> // import std::function
 #include <vector> // import std::vector
 
-#include "console.h" // import console
-
-using callback = std::function<void()>;
+using callback_t = std::function<void()>;
 
 class EventLoop {
   
   private:
-    bool                  running = false;
-    std::vector<callback> polls;
-    // std::vector<callback> poll_callbacks;
+    bool                    running = false;
+    std::vector<callback_t> polls;
 
   void run_polls() {
-    // poll_callbacks.clear();
-    for (callback & cb : polls) {
+    for(callback_t & cb : polls) {
       cb();
     }
-    // for (callback & cb : poll_callbacks) {
-    //   cb();
-    // }
   }
 
   void loop() {
@@ -33,13 +25,9 @@ class EventLoop {
   }
 
   public:
-    void push_poll(callback cb) {
+    void push_poll(callback_t cb) {
       polls.push_back(cb);
     }
-
-    // void push_poll_callback(callback cb) {
-    //   poll_callbacks.push_back(cb);
-    // }
 
     void start() {
       running = true;
