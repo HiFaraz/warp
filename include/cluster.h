b9ed77b3b2122f9654b2d2f516b4cacb9f841529
Faraz Syed <hello@farazsyed.com>
2018-02-24 16:13:56 -0500

    feat: add concurrency with forks

---
+++ include/cluster.h
@@ -0,0 +1,48 @@
+#ifndef CLUSTER_H
+#define CLUSTER_H
+
+#include <functional> // import std::function
+#include <thread> // import std::thread
+#include <unistd.h> // import fork
+#include <vector> // import std::vector
+
+/**
+ * Cluster supports both simple forks and threads
+ * 
+ * cluster::fork(int num_forms)
+ * cluster::threads::launch(std::function<void()> task)
+ */
+
+namespace cluster {
+
+  void fork(int num_forks) {
+    // use the parent process, so actually create num_forks - 1
+    int fork_count = 1; // account for the parent process
+    while(fork_count < num_forks) {
+      pid_t pid = ::fork();
+      if (pid > 0) {
+        fork_count++;
+      } else if (pid == 0) {
+        break;
+      }
+    }
+  }
+
+  namespace threads {
+    std::vector<std::thread> list{std::thread::hardware_concurrency()};
+
+    void launch(std::function<void()> task) {
+      console::log("Launching " + std::to_string(list.size()) + " threads");
+
+      // create n-1 threads (use the master thread too)
+      for(unsigned index = 1; index < list.size(); ++index) {
+          list[index] = std::thread(task);
+      }
+
+      task(); // from the master thread
+    }
+  }
+
+}
+
+#endif // !CLUSTER_H
--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,11 @@
 #include <cstdlib> // import std::getenv
 #include <iostream>
 #include <string>
-// #include <thread> // import std::thread
-// #include <vector> // import std::vector
-// #include <unistd.h> // import fork
 
 #include "console.h" // import console
 #include "event-loop.h" // import EventLoop
+#include "cluster.h" // import cluster (namespace)
 #include "server.h" // import Server, ClientSocket
-
 
 void on_client_event(int client_fd) {
   ClientSocket socket{client_fd};
@@ -23,7 +20,7 @@ void on_client_event(int client_fd) {
   } while (chunkSize > 0);
   // std::cout << "last chunk " << chunkSize << " bytes" << std::endl;
   // std::cout << "buf " << bufferSize << " bytes" << std::endl;
-  // std::cout << "buf\n" << buffer << std::endl;
+  // std::cout << buffer;
 
   // if (bufferSize > 0) {
     char hello[] = "HTTP/1.1 200 OK\r\n\r\nHello from myself\r\n";
@@ -31,7 +28,7 @@ void on_client_event(int client_fd) {
     // std::cout << "sent " << sent << " bytes" << std::endl;
   // }
 
-  socket.close();
+  socket.close(); // TODO support persistent connections and pipelining
 }
 
 void start_server() {
@@ -46,33 +43,15 @@ void start_server() {
     port = std::stoi(env_port);
   }
   server.listen(port);
-  console::log("listening on port " + std::to_string(port));
+  console::log("Listening on port " + std::to_string(port));
   
   event_loop.start();
 }
 
 int main(int argc, char const *argv[]) {
-  // int children_count = 0;
-  // pid_t child_pid = fork();
 
-  // if (child_pid > 0) {
-  //   console::log("parent process forked");
-  //   children_count++;
-  // } else if (child_pid == -1) {
-  //   console::log("parent failed to fork");
-  // } else {
-  //   console::log("child process start");
-  // }
-
-  // std::vector<std::thread> threads(std::thread::hardware_concurrency());
-
-  // console::log(threads.size());
-
-  // // create n-1 threads (use the master thread for serving requests too)
-  // for(unsigned i = 1; i < threads.size(); ++i) {
-  //     threads[i] = std::thread(start_server);
-  // }
-
+  // cluster::threads::launch(start_server);
+  cluster::fork(4);
   start_server();
   
   return EXIT_SUCCESS;
