08cedaf39a40ed1a8df1e41aebdb95dc4279c14b
Faraz Syed <hello@farazsyed.com>
2018-02-26 01:49:22 -0500

    feat(buffer): add append method and << operator overload

--- include/photon/buffer.h
+++ include/photon/buffer.h
@@ -2,8 +2,9 @@
 #define PHOTON_BUFFER_H
 
 #include <cstddef> // size_t
-#include <cstring> // memset
+#include <cstring> // memcpy, memset
 #include <iostream> // std::ostream
+#include <stdexcept>
 
 class Buffer {
   
@@ -15,11 +16,25 @@ class Buffer {
     Buffer(std::size_t capacity) {
       data = new char[capacity];
       this->capacity = capacity;
-      this->size = capacity;
+      size = 0;
     }
 
     Buffer (std::size_t size, int filler) : Buffer(size) {
       fill(filler);
+    }
+
+    void append(const char* input) {
+      size_t input_size = strlen(input);
+      size_t free_space = capacity - size;
+      if (free_space < input_size) {
+        throw std::runtime_error{"Not enough space in Buffer, flush the buffer first"};
+      }
+      memcpy(data + size, input, input_size);
+      size += input_size;
+    }
+
+    void empty() {
+      size = 0;
     }
 
     void fill(int filler) {
@@ -29,11 +44,16 @@ class Buffer {
     char operator[] (std::size_t index) {
       return data[index];
     }
+
+    Buffer& operator<<(const char* input) {
+      append(input);
+      return *this;
+    }
 };
 
 std::ostream& operator<<(std::ostream& os, const Buffer& buffer) {  
-    os << buffer.data;
-    return os;  
-}  
+  os << buffer.data;
+  return os;  
+}
 
 #endif // !PHOTON_BUFFER_H
